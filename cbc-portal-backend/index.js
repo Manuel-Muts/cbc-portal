5c11a33b2da8eef6392300dc289cd82a3ff420d9
Manuel-Muts <gitongaemmanuel50@gmail.com>
2026-02-16 21:23:52 +0300

    update the CORS in index.js

--- cbc-portal-backend/index.js
+++ cbc-portal-backend/index.js
@@ -40,9 +40,9 @@ app.use(helmet());
 // CORS
 // -------------------------
 const FRONTEND_ORIGINS = [
-  "http://127.0.0.1:5500",
-  "http://localhost:5500",
   "https://competence-hub.onrender.com",
+
+  
   process.env.FRONTEND_URL
 ];
 
